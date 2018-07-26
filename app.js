39aac2ab31790f89af25983ab69048a7a6ae7cd3
pedri77 <dmgmoya85@gmail.com>
2018-07-26 08:34:12 +0200

    changes app.js

--- app.js
+++ app.js
@@ -6,22 +6,47 @@ var express = require("express"),
     methodOverride = require("method-override");
     mongoose = require('mongoose');
 
+//Conection to DB
+
+mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
+  if(err) throw err;
+  console.log('Connected to Database');
+});
+
+// Middlewares
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+//API routes
+
+var tvshows = express.Router();
+
+tvshows.route('/tvshows')
+  .get(TVShowCtrl.findAllTVShows)
+  .post(TVShowCtrl.addTVShow);
+
+tvshows.route('/tvshows/:id')
+  .get(TVShowCtrl.findById)
+  .put(TVShowCtrl.updateTVShow)
+  .delete(TVShowCtrl.deleteTVShow);
+
+app.use('/api', tvshows);
+
+// Example route
 var router = express.Router();
 
 router.get('/', function(req, res) {
    res.send("Hello World!");
 });
 
+//Start server
+
 app.use(router);
 
-mongoose.connect('mongodb://localhost/tvshows', function(err, res) {
-  if(err) {
-    console.log('ERROR: connecting to Database. ' + err);
-  }
-  app.listen(3000, function() {
+//Start server
+
+app.listen(3000, function() {
     console.log("Node server running on http://localhost:3000");
   });
